2e975adce79fc9246a6bc38e1ef44d6c9ebc14af
Greem4 <i@greem.ru>
2026-07-17 12:49:50 +0300

    Обновил версию приложения до 0.6.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 2
-        versionName = "0.6.8.0"
+        versionName = "0.6.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
