b2fb4a92841c63c1fccf98e4335327266d64adfd
Greem4 <i@greem.ru>
2026-07-27 21:56:09 +0300

    верися 0.6.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 3
-        versionName = "0.7.0.0"
+        versionName = "0.6.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
